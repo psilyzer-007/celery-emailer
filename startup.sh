67d68d5290b5ecbc4865d4fa569294dc0d5b278e
Sai Sreyas Ray <s.sai@iitg.ac.in>
2022-06-30 11:48:40 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,4 +2,4 @@
 export LANG=C.UTF-8
 
 
-gunicorn — bind=0.0.0.0 — timeout 600 --chdir core core.wsgi & celery -A core worker -l INFO & celery -A core beat -l INFO 
+gunicorn — bind=0.0.0.0 — timeout 600 core.wsgi & celery -A core worker -l INFO & celery -A core beat -l INFO 
